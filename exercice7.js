0c3ccc19f36e066abe624ebacff4aa0952a7737c
fatimakali <bakook20gmail.com>
2023-11-21 22:11:01 +0100

    Exercice 7

---
+++ exercice7.js
@@ -0,0 +1,10 @@
+function addition(nb1, nb2) {
+    result = nb1 + nb2;
+    return result
+}
+
+var n = parseInt(prompt("Entrez une valeur"));
+var result = 0;
+addition(nb1, nb2)
+
+alert( "La valeur est égale à " + result);
